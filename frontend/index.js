54e7e7e46d0e5d712001b188589806f8a8ef4c21
skyechibi <163659571+skyechibi@users.noreply.github.com>
2024-03-17 18:41:12 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -171,6 +171,5 @@ function upd() {
   console.log({htm, page})
 
   //dom.iframe.code.style.textContent = css;
-
-  dom.iframe.code.elem.src = page;
+  document.getElementById('editor').src = page;
 }
